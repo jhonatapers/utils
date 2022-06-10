6d50684f696dd77914a8f90462e5f52ee893df2e
Jhonata Peres <jhonatapers@gmail.com>
2022-06-10 14:00:24 -0300

    Update CsvUtils.cs

--- CsvUtils.cs
+++ CsvUtils.cs
@@ -1,6 +1,7 @@
     public static class Csv
     {
 
+
         public enum lineSplitter
         {
             CRLF,
@@ -23,8 +24,6 @@
             Stopwatch timeSpend = new Stopwatch();
             timeSpend.Start();
 
-            columnSplitter = columnSplitter + "(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-
             DataTable dt = new DataTable();
             try
             {
@@ -44,36 +43,32 @@
                 }
 
                 string[] allLines = getLines(encoding.GetString(bFile), _lineSplitter);
+                int pointer = 0;
 
                 if (allLines.Length == 0)
                     return dt;
 
-                string firstLine = Regex.Replace(allLines[0].Replace("\u001a", ""), "^(\n)", "");
-
                 if (containHeader)
                 {
-                    getHeader(firstLine, dt, columnSplitter);
+                    bool headerSet = false;
+
+                    while (!headerSet && pointer < allLines.Length) 
+                    {
+                        headerSet = setHeader(allLines[pointer].Replace("\u001a", ""), dt, columnSplitter);
+                        pointer++;
+                    }
                 }
                 else
                 {
-                    getColumns(firstLine, dt, columnSplitter);
-
-                    string[] row = Regex.Split(firstLine, columnSplitter);
-
-                    DataRow dr = dt.NewRow();
-                    for (int i = 0; i < row.Length; i++)
-                    {
-                        dr[i] = row[i];
-                    }
-                    dt.Rows.Add(dr);
+                    setColumns(allLines[pointer].Replace("\u001a", ""), dt, columnSplitter);
                 }
 
-                for (int i = 1; i < allLines.Length; i++)
+                for (int i = pointer; i < allLines.Length; i++)
                 {
-                    string line = Regex.Replace(allLines[i].Replace("\u001a", ""), "^(\n)", "");
+                    string line = allLines[i].Replace("\u001a", "");
                     if (!line.Equals(""))
                     {
-                        string[] row = Regex.Split(line, columnSplitter);
+                        string[] row = getColumns(line, columnSplitter);
 
                         DataRow dr = dt.NewRow();
                         for (int j = 0; j < row.Length; j++)
@@ -104,8 +99,8 @@
             int startPosition = 0;
             bool isInQuotes = false;
             for (int currentPosition = 0; currentPosition < csv.Length; currentPosition++)
-            {
-                if (csv.Length <= currentPosition + jump)
+            {                
+                if (csv.Length < currentPosition + jump)
                     break;
                 
                 if (csv[currentPosition] == '\"')
@@ -116,6 +111,7 @@
                 {
                     csvLines.Add(csv.Substring(startPosition, (currentPosition) - startPosition));
                     startPosition = currentPosition + jump;
+                    currentPosition += jump - 1;
                 }
             }
 
@@ -124,10 +120,44 @@
             return csvLines.ToArray();
         }
 
-
-        private static void getColumns(String firstLine, DataTable dt, string splitter)
+        private static String[] getColumns(String csvLine, String splitter)
         {
-            string[] firstRow = Regex.Split(firstLine, splitter);
+            List<String> csvColumns = new List<String>();
+            int startPosition = 0;
+            int jump = splitter.Length;
+            bool isInQuotes = false;
+            for (int currentPosition = 0; currentPosition < csvLine.Length; currentPosition++)
+            {
+                if (csvLine.Length < currentPosition + jump)
+                    break;
+
+                if (csvLine[currentPosition] == '\"')
+                {
+                    isInQuotes = !isInQuotes;
+                }
+                else if (csvLine.Substring(currentPosition, jump).Equals(splitter) && !isInQuotes)
+                {
+                    csvColumns.Add(csvLine.Substring(startPosition, (currentPosition) - startPosition));
+                    startPosition = currentPosition + jump;
+                    currentPosition += jump -1;
+                }
+            }
+
+            if (csvLine.Substring(startPosition).Equals(splitter))
+            {
+                csvColumns.Add("");
+            }
+            else
+            {
+                csvColumns.Add(csvLine.Substring(startPosition));
+            }
+
+            return csvColumns.ToArray();
+        }
+
+        private static void setColumns(String firstLine, DataTable dt, string splitter)
+        {
+            string[] firstRow = getColumns(firstLine, splitter);
 
             for (int i = 0; i < firstRow.Length; i++)
             {
@@ -135,33 +165,30 @@
             }
         }
 
-        private static void getHeader(String firstLine, DataTable dt, string splitter)
+        private static bool setHeader(String firstLine, DataTable dt, string splitter)
         {
-            while (true)
+            bool isHeader = false;
+            string[] headers = getColumns(firstLine, splitter);
+            foreach (string header in headers)
             {
-                bool isHeader = false;
-                string[] headers = Regex.Split(firstLine, splitter);
-                foreach (string header in headers)
+                if (header.Equals(""))
                 {
-                    if (header.Equals(""))
-                    {
-                        isHeader = false;
-                        break;
-                    }
-
-                    isHeader = true;
+                    isHeader = false;
+                    break;
                 }
 
-                if (isHeader)
-                {
-                    for (int i = 0; i < headers.Length; i++)
-                    {
-                        dt.Columns.Add(i.ToString() + "_" + headers[i].Trim('"'));
-                    }
+                isHeader = true;
+            }
 
-                    return;
+            if (isHeader)
+            {
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    dt.Columns.Add(i.ToString() + "_" + headers[i].Trim('"'));
                 }
             }
+
+            return isHeader;
         }
 
         public static string ReplaceAll(this string original, string[] toBeReplaced, string newValue)
@@ -172,6 +199,46 @@
                 if (!string.IsNullOrEmpty(str))
                     original = original.Replace(str, newValue);
             return original;
+        }
+
+        public static void convertDataTableToCSV(DataTable dataTable, string strFilePath, string splitter, Encoding encoding)
+        {
+            StreamWriter sw = new StreamWriter(strFilePath, false, encoding);
+            //headers    
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                sw.Write(dataTable.Columns[i]);
+                if (i < dataTable.Columns.Count - 1)
+                {
+                    sw.Write(splitter);
+                }
+            }
+            sw.Write(sw.NewLine);
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                for (int i = 0; i < dataTable.Columns.Count; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        string value = dr[i].ToString();
+                        if (value.Contains(splitter))
+                        {
+                            value = String.Format("\"{0}\"", value);
+                            sw.Write(value);
+                        }
+                        else
+                        {
+                            sw.Write(dr[i].ToString());
+                        }
+                    }
+                    if (i < dataTable.Columns.Count - 1)
+                    {
+                        sw.Write(splitter);
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
         }
 
     }
